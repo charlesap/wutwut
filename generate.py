ad40a319793f999a7a04f9204c3e6ec2f02bcd50
Charles Perkins <chuck@kuracali.com>
2018-01-29 21:30:16 -0800

    add rough generate functionality

--- generate.py
+++ generate.py
@@ -1,19 +1,58 @@
 import yaml
-document = """
-  a: 1
-  b:
-    c: 3
-    d: 4
-"""
-print(yaml.dump(yaml.load(document)))
+import os
+from string import Template
+
+with open("Projects.yaml", 'r') as stream:
+    try:
+        projects = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit()
+
+with open("Encoders.yaml", 'r') as stream:
+    try:                                  
+        encoders = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc) 
+        exit()                       
+
+with open("Languages.yaml", 'r') as stream:
+    try:                                  
+        languages = yaml.load(stream)      
+    except yaml.YAMLError as exc:         
+        print(exc)                        
+        exit()                      
+
+for p in projects.items():
+    print("\n\n---",p[0]) 
+    d=p[1] 
+    lang=d['Language']
+    loc=d['Path']
+    src=os.path.join(loc,d['SourceSubPath'])
+    tst=os.path.join(loc,d['TestsSubPath'])
+    if not os.path.exists(loc):
+      os.makedirs(loc)
+    if not os.path.exists(src):
+      os.makedirs(src)
+    if not os.path.exists(tst):
+      os.makedirs(tst)
+    mfn=d['MainFileName']
+    mfloc=os.path.join(loc,d['MainFileSubPath'],mfn)
+    if not os.path.exists(mfloc):
+      print("-->",mfloc," does not exist.")
+      open(mfloc,'a').close()
+    fl=open(mfloc).readline().rstrip()
+    l=languages[d['Language']]
+    modstmt = Template(l['ModuleStart']).substitute(mn=d['MainModuleName'])
+    print(modstmt)
+    mainstmt = Template(l['MainStart']).substitute(mn=d['MainModuleName'])
+    print(mainstmt)                                                         
+    mainendstmt = Template(l['MainEnd']).substitute(mn=d['MainModuleName'])
+    print(mainendstmt)                                                         
+    modendstmt = Template(l['ModuleEnd']).substitute(mn=d['MainModuleName'])
+    print(modendstmt)                                                         
 
 
-"""
- - Language: Julia
- - Language: Go
- - Language: Python
- - Language: Java
- - Language: c
- - Language: vhdl
- - Language: Haskell
-"""
+#for e in encoders.items():
+#    print(e[0])
+#    d=e[1]
